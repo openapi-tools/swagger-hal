95b7deaddc0b7ca974530a73790d4c2950991fd4
Thor Anker Kvisgård Lange <tal@nykredit.dk>
2017-01-01 22:58:55 +0100

    Adding some assertions to the test.

--- src/test/java/dk/nykredit/swagger/HALModelConverterIT.java
+++ src/test/java/dk/nykredit/swagger/HALModelConverterIT.java
@@ -1,10 +1,13 @@
 package dk.nykredit.swagger;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import dk.nykredit.swagger.example.AccountServiceExposure;
 import io.swagger.jaxrs.Reader;
 import io.swagger.jaxrs.config.ReflectiveJaxrsScanner;
+import io.swagger.models.Model;
 import io.swagger.models.Swagger;
-import io.swagger.util.Yaml;
 import org.junit.Test;
 
 public class HALModelConverterIT {
@@ -16,9 +19,11 @@ public class HALModelConverterIT {
         reflectiveScanner.setResourcePackage(AccountServiceExposure.class.getPackage().getName());
         Reader reader = new Reader(swagger);
         swagger = reader.read(reflectiveScanner.classes());
-        System.out.println(swagger);
-        Yaml.prettyPrint(swagger);
-        
+        assertTrue(swagger.getDefinitions().containsKey("AccountRepresentation"));
+        Model accountRepresentation = swagger.getDefinitions().get("AccountRepresentation");
+        assertTrue(accountRepresentation.getProperties().containsKey("_links"));
+        assertTrue(accountRepresentation.getProperties().containsKey("_embedded"));
+        assertEquals(5, accountRepresentation.getProperties().size());
     }
-    
+
 }
